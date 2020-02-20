c0efd73836ba7ed18b309767cd87642cc5ea89f2
raberin <rmvaberin@gmail.com>
2020-02-19 19:25:02 -0800

    Added edit button

--- src/components/Drug.js
+++ src/components/Drug.js
@@ -3,36 +3,69 @@ import axios from "axios";
 
 import "./Drugs.css";
 
-const Drug = props => {
-  const deleteDrug = () => {
+class Drug extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      editButtonClicked: false,
+      updatedPost: {
+        name: "",
+        iupac_name: "",
+        smiles: "",
+        molecular_formula: "",
+        cas_number: "",
+        mw: "",
+        class: "",
+        superclass: "",
+        rotateable_bond_count: "",
+        complexity: ""
+      }
+    };
+  }
+
+  deleteDrug = () => {
     axios
-      .delete(`http://localhost:5000/api/drugs/${props.id}`)
+      .delete(`http://localhost:5000/api/drugs/${this.props.id}`)
       .then(res => {
         console.log(res);
-        props.fetchDrugs();
-        alert(`${props.name} has been deleted!`);
+        this.props.fetchDrugs();
+        alert(`${this.props.name} has been deleted!`);
       })
       .catch(err => {
         console.log(err);
       });
   };
 
-  return (
-    <div className="drugCard">
-      <div className="drugButtons">
-        <span className="x-button" onClick={deleteDrug}>
-          X
-        </span>
+  updateDrug = updatedPost => {
+    // Updates the current item using the updatedPost in state
+    axios
+      .put(`http://localhost:5000/api/drugs/${this.props.id}`, updatedPost)
+      .then(res => {
+        this.props.fetchDrugs();
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  };
+  render() {
+    return (
+      <div className="drugCard">
+        <div className="drugButtons">
+          <span onClick={this.updateDrug}>Edit</span>
+          <span className="x-button" onClick={this.deleteDrug}>
+            X
+          </span>
+        </div>
+        <div className="drugContent">
+          <h3>{this.props.name}</h3>
+          <p>Molecular Formula: {this.props.formula}</p>
+          <p>IUPAC: {this.props.iupac}</p>
+          <p>SMILES: {this.props.smiles}</p>
+          <p>Molecular Weight: {this.props.mw}</p>
+        </div>
       </div>
-      <div className="drugContent">
-        <h3>{props.name}</h3>
-        <p>Molecular Formula: {props.formula}</p>
-        <p>IUPAC: {props.iupac}</p>
-        <p>SMILES: {props.smiles}</p>
-        <p>Molecular Weight: {props.mw}</p>
-      </div>
-    </div>
-  );
-};
+    );
+  }
+}
 
 export default Drug;
